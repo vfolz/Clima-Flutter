a0d4dedbd13498e8fd0ee2c29af72487f82d0776
vfolz <victorfolz@gmail.com>
2020-03-10 10:52:43 -0700

    Added location requests and getLocation method

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
+
+void getLocation () async {
+  Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+  print(position);
+}
+
 
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
@@ -12,7 +19,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
       body: Center(
         child: RaisedButton(
           onPressed: () {
-            //Get the current location
+              getLocation();
           },
           child: Text('Get Location'),
         ),
